b05abec7115cbe70ba0c08da1ad78e892c7c8091
sivasankarp95 <spalania@ucsd.edu>
2019-11-14 20:32:29 -0800

    Added script for spmv

---
+++ spmv/vivado/script/copy_syn_reports.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os, sys
+import glob
+import shutil
+import re
+
+syn_dir = 'syn_reports'
+
+try:
+    os.mkdir(syn_dir)
+except:
+    pass
+
+for d in sorted(glob.glob('solutions/spmv*')):
+    m = re.search('spmv(\d+)', d)
+    num = m.group(1)
+    try:
+        for f in glob.glob(d + "/spmv/solutionx/syn/report/*"):
+            basename, ext = os.path.splitext(os.path.basename(f))
+            shutil.copy(f, os.path.join(syn_dir, basename + num + ext))
+    except:
+        print("Error in #" + num)
+
