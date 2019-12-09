0b1314f87f346653d55f3ac7f3bad4f0033e2310
mdkul22 <mdkul22@gmail.com>
2019-12-08 20:43:18 -0800

    cat: normals(nw) and matmul (w)

---
+++ normals/catapult/src/normals.h
@@ -0,0 +1,15 @@
+#ifdef NORMALS_H
+#define NORMALS_H
+#include <ac_int.h>
+#include <ac_channel.h>
+#include <ac_fixed.h>
+
+#include "params.h"
+
+typedef ac_fixed<20, 8, true> fl;
+typedef ac_int<32, true> dint32
+
+struct DATA_MEM {
+	fl data[TVAL];
+};
+#endif
