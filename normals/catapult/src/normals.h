7c640be8b34133a16d6d585c7107089dd13a63aa
mdkul22 <mdkul22@gmail.com>
2019-12-17 23:27:21 -0800

    removing Catapultnfolders from matmult/catapult/.

--- normals/catapult/src/normals.h
+++ normals/catapult/src/normals.h
@@ -1,4 +1,4 @@
-#ifdef NORMALS_H
+#ifndef NORMALS_H
 #define NORMALS_H
 #include <ac_int.h>
 #include <ac_channel.h>
@@ -7,7 +7,7 @@
 #include "params.h"
 
 typedef ac_fixed<20, 8, true> fl;
-typedef ac_int<32, true> dint32
+typedef ac_int<32, true> dint32;
 
 struct DATA_MEM {
 	fl data[TVAL];
