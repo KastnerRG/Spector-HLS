75ac99fcb461be6ae48b6c5be916c5c69f51a5be
codspalaniappan95 <spalania@ucsd.edu>
2019-12-23 16:20:41 -0800

    Added modified files for spmv vivado

--- spmv/vivado/float/script/gen_pnr.tcl
+++ spmv/vivado/float/script/gen_pnr.tcl
@@ -7,7 +7,7 @@ add_files -tb my_spmv_common.h
 add_files -tb csrmatrix_R1_N512_D5000_S01
 #Solution
 open_solution -reset "solutionx"
-set_part {xc7z020clg400-1} -tool vivado
+set_part {xcvu9p-flga2104-1-e} -tool vivado
 create_clock -period 10
 
 #source "./histogram/solution1/directives.tcl"
