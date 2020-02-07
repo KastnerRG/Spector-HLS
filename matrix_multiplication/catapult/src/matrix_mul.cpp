7680a102d9477bec4b089fe9da5d0078b5bcd27f
mdkul22 <mdkul22@gmail.com>
2020-02-06 21:30:18 -0800

    adding xcvu9p notebooks and csv files in master

--- matrix_multiplication/catapult/src/matrix_mul.cpp
+++ matrix_multiplication/catapult/src/matrix_mul.cpp
@@ -2,31 +2,35 @@
 #include "matrix_mul.h"
 
 #pragma design top
-void matrix_mul(ac_channel<dtype> &A, dtype B[TMVAL], dtype C[TMVAL])
+void matrix_mul(ac_channel<dtype> &A, ac_channel<dtype> &B, ac_channel<dtype> &C)
 {
-	DATA_MEM mem;
+	DATA_MEM memA, memB, memC;
   #ifndef __SYNTHESIS__
 	while(A.available(TMVAL))
 	#endif
 	{
 		LOAD_MEM_MAIN: for(unsigned i=0; i<TMVAL; i++){
-			mem.data[i] = A.read();
+			memA.data[i] = A.read();
+			memB.data[i] = B.read();
+			memC.data[i] = 0;
 		}
 		loop1:for(int i=0;i<TMVAL;i=i+SUBDIM_X)
 		{
 			loop2:for(int p=0;p<SUBDIM_X;p++)
 			{
 				dtype temp;
-				temp=mem.data[MVAL*i+p];
+				temp=memA.data[MVAL*i+p];
 				int t=(i+p)>>int(Mlog);
 				int r=(i+p)%MVAL;
 				loop3:for(int j=0;j<MVAL;j=j+SUBDIM_Y)
 				{
 					loop4:for(int l=0;l<SUBDIM_Y;l++)
-						C[t*MVAL+j+l]=C[t*MVAL+j+l]+temp*B[r*MVAL+j+l];
+						memC.data[t*MVAL+j+l]=memC.data[t*MVAL+j+l]+temp*memB.data[r*MVAL+j+l];
 				}
 			}
 		}
+		loop_5:for(int i=0; i<MVAL; i++)
+						 C.write(memC.data[i]);
 	}
 }
 
