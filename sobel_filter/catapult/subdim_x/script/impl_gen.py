c4037f204f9a0141576512c8959ee136f6e0539c
codspalaniappan95 <spalania@ucsd.edu>
2020-02-21 19:37:05 +0000

    Updated scripts

---
+++ sobel_filter/catapult/subdim_x/script/impl_gen.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os, sys
+import glob
+import shutil
+import re
+import subprocess
+import multiprocessing as mp
+import time
+import datetime
+
+logName = "pnr.log"
+file_list=[]
+
+file_list=glob.glob('rtl_files/sobel_subdimx*')
+#print(file_list)
+compiled = []
+try:
+    os.mkdir('impl')
+except:
+    pass
+def run_script(path):
+    print(path, "started at", datetime.datetime.now())
+    try:
+        m = re.search('sobel_subdimx(\d+)', path)
+        if os.path.isdir('impl/'+m[0]):
+            pass
+        else:
+            os.makedirs('impl/'+m[0])
+        shutil.copy(path+'/'+'rtl.v','impl/'+m[0]+'/')
+        shutil.copy(path+'/'+'concat_rtl.v','impl/'+m[0]+'/')
+        shutil.copy('vivado_mentor.tcl','impl/'+m[0]+'/')
+        shutil.copy('extraction.tcl','impl/'+m[0]+'/')
+        shutil.copy('constr.xdc','impl/'+m[0]+'/')
+        '''
+        with open('impl/'+m[0]+'/'+'vivado_mentor.tcl', "r") as fin:
+            text=''
+            for line in fin:
+                newline=line
+                newline=line.replace("%1",'impl/'+m[0])
+                text=text+newline
+        with open('impl/'+m[0]+'/'+'vivado_mentor.tcl', "w") as fin:
+                fin.writelines(text)
+        '''
+        start = time.time()
+        command = " ".join(["timeout 7200","vivado","-mode","batch","-source","./vivado_mentor.tcl"])
+        subprocess.check_output(command, cwd='impl/'+m[0], shell=True)
+        end = time.time()
+
+        outFile = open(logName, 'at')
+        outFile.write(path + "\n")
+        outFile.close()
+
+        outFile = open(logName + ".time", 'at')
+        outFile.write(path + ',' + str(end-start) + '\n')
+        outFile.close()
+
+        print(path, "end at", datetime.datetime.now(), " elapsed", end-start)
+	
+    except subprocess.CalledProcessError:
+        print(path, "Timeout at", datetime.datetime.now())
+
+        outFile = open(logName + '.timeout', 'at')
+        outFile.write(path + '\n')
+        outFile.close()
+
+    except:
+        raise
+
+
+def main():
+    if os.path.isfile(logName):
+        print("Reading processed folders from log file")
+        logFile = open(logName, 'rt')
+        for line in logFile:
+            name = line.split('\n')
+            compiled.append(name[0])
+        logFile.close()
+    for f in compiled:
+        file_list.remove(f)
+
+
+
+#print(file_list)
+    pool = mp.Pool()
+    result = pool.map_async(run_script, file_list).get(31536000)
+
+if __name__=="__main__":
+    main()
---
+++ sobel_filter/catapult/subdim_y/script/copy_syn_reports_rtl_code.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os, sys
+import glob
+import shutil
+import re
+
+syn_dir = 'syn_reports'
+rtl='rtl_files'
+try:
+    os.mkdir(syn_dir)
+    os.mkdir(rtl)
+except:
+    pass
+
+for d in sorted(glob.glob('solutions/sobel_subdimy*')):
+    m = re.search('sobel_subdimy(\d+)', d)
+    num = m.group(1)
+    try:
+        for f in glob.glob(d + "/Catapult/sobel_y.v1/cycle.rpt"):
+            basename, ext = os.path.splitext(os.path.basename(f))
+            shutil.copy(f, os.path.join(syn_dir,basename+num+ext))
+    except:
+        print("Error in #" + num)
+    try:
+        for f in glob.glob(d + "/Catapult/sobel_y.v1/"):
+            t=rtl+'/'+m.group(0)+'/'
+            if (os.path.isfile(f+'rtl.v') and os.path.isfile(f+'concat_rtl.v')):
+                os.makedirs(t) 
+                shutil.copy(f+'rtl.v',t)
+                shutil.copy(f+'concat_rtl.v',t)
+            else:
+                pass
+    except:
+        pass
+
+
