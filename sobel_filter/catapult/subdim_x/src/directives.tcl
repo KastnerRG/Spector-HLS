dd5b915694d044273cb6e011af4228671decf8d7
mdkul22 <mdkul22@gmail.com>
2020-02-26 21:50:59 -0800

    Modifying sobelx, removing ac_channel opts

--- sobel_filter/catapult/subdim_x/src/directives.tcl
+++ sobel_filter/catapult/subdim_x/src/directives.tcl
@@ -5,7 +5,7 @@
 //             CONFIDENTIAL AND PROPRIETARY INFORMATION WHICH IS THE
 //                 PROPERTY OF MENTOR GRAPHICS OR ITS LICENSORS
 //  
-//  Running on Linux mdk@mdk-FX504 5.0.0-37-generic x86_64 aol
+//  Running on Linux mdk@mdk-FX504 5.3.0-28-generic x86_64 aol
 //  
 //  Package information: SIFLIBS v23.4_2.0, HLS_PKGS v23.4_2.0, 
 //                       SIF_TOOLKITS v23.4_2.0, SIF_XILINX v23.4_2.0, 
@@ -18,57 +18,55 @@ solution new -state initial
 solution options defaults
 solution options set /Input/CppStandard c++11
 solution file add ./sobel_subdimx.cpp -type C++
-directive set -DESIGN_GOAL area
-directive set -SPECULATE true
-directive set -MERGEABLE true
-directive set -REGISTER_THRESHOLD 256
-directive set -MEM_MAP_THRESHOLD 32
-directive set -LOGIC_OPT false
-directive set -FSM_ENCODING none
-directive set -FSM_BINARY_ENCODING_THRESHOLD 64
-directive set -REG_MAX_FANOUT 0
-directive set -NO_X_ASSIGNMENTS true
-directive set -SAFE_FSM false
-directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
-directive set -REGISTER_SHARING_LIMIT 0
-directive set -ASSIGN_OVERHEAD 0
-directive set -TIMING_CHECKS true
-directive set -MUXPATH true
-directive set -REALLOC true
-directive set -UNROLL no
-directive set -IO_MODE super
-directive set -CHAN_IO_PROTOCOL use_library
-directive set -ARRAY_SIZE 1024
-directive set -REGISTER_IDLE_SIGNAL false
-directive set -IDLE_SIGNAL {}
-directive set -STALL_FLAG false
-directive set -TRANSACTION_DONE_SIGNAL true
-directive set -DONE_FLAG {}
-directive set -READY_FLAG {}
-directive set -START_FLAG {}
-directive set -RESET_CLEARS_ALL_REGS use_library
-directive set -CLOCK_OVERHEAD 20.000000
-directive set -OPT_CONST_MULTS use_library
-directive set -CHARACTERIZE_ROM false
-directive set -PROTOTYPE_ROM true
-directive set -ROM_THRESHOLD 64
-directive set -CLUSTER_ADDTREE_IN_WIDTH_THRESHOLD 0
-directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
-directive set -CLUSTER_OPT_CONSTANT_INPUTS true
-directive set -CLUSTER_RTL_SYN false
-directive set -CLUSTER_FAST_MODE false
-directive set -CLUSTER_TYPE combinational
 directive set -PIPELINE_RAMP_UP true
+directive set -CLUSTER_TYPE combinational
+directive set -CLUSTER_FAST_MODE false
+directive set -CLUSTER_RTL_SYN false
+directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+directive set -CLUSTER_ADDTREE_IN_WIDTH_THRESHOLD 0
+directive set -ROM_THRESHOLD 64
+directive set -PROTOTYPE_ROM true
+directive set -CHARACTERIZE_ROM false
+directive set -OPT_CONST_MULTS use_library
+directive set -CLOCK_OVERHEAD 20.000000
+directive set -RESET_CLEARS_ALL_REGS use_library
+directive set -START_FLAG {}
+directive set -READY_FLAG {}
+directive set -DONE_FLAG {}
+directive set -TRANSACTION_DONE_SIGNAL true
+directive set -STALL_FLAG false
+directive set -IDLE_SIGNAL {}
+directive set -REGISTER_IDLE_SIGNAL false
+directive set -ARRAY_SIZE 1024
+directive set -CHAN_IO_PROTOCOL use_library
+directive set -IO_MODE super
+directive set -UNROLL no
+directive set -REALLOC true
+directive set -MUXPATH true
+directive set -TIMING_CHECKS true
+directive set -ASSIGN_OVERHEAD 0
+directive set -REGISTER_SHARING_LIMIT 0
+directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
+directive set -SAFE_FSM false
+directive set -NO_X_ASSIGNMENTS true
+directive set -REG_MAX_FANOUT 0
+directive set -FSM_BINARY_ENCODING_THRESHOLD 64
+directive set -FSM_ENCODING none
+directive set -LOGIC_OPT false
+directive set -MEM_MAP_THRESHOLD 32
+directive set -REGISTER_THRESHOLD 256
+directive set -MERGEABLE true
+directive set -SPECULATE true
+directive set -DESIGN_GOAL latency
 go new
 directive set -DESIGN_HIERARCHY sobel_x
 go compile
-solution library add mgc_Xilinx-VIRTEX-uplus-2_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family VIRTEX-uplus -speed -2 -part xcvu13p-flga2577-2-i
+solution library add mgc_Xilinx-VIRTEX-uplus-1_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family VIRTEX-uplus -speed -1 -part xcvu9p-flgb2104-1-e
 solution library add Xilinx_RAMS
 solution library add Xilinx_ROMS
 solution library add Xilinx_FIFO
 go libraries
 directive set -CLOCKS {clk {-CLOCK_PERIOD 10.0 -CLOCK_EDGE rising -CLOCK_UNCERTAINTY 0.0 -CLOCK_HIGH_TIME 5.0 -RESET_SYNC_NAME rst -RESET_ASYNC_NAME arst_n -RESET_KIND sync -RESET_SYNC_ACTIVE high -RESET_ASYNC_ACTIVE low -ENABLE_ACTIVE high}}
 go assembly
-directive set /sobel_x/core/loop_1 -UNROLL 2
-go architect
 go extract
--- sobel_filter/catapult/subdim_y/src/sobel_subdimy.cpp
+++ sobel_filter/catapult/subdim_y/src/sobel_subdimy.cpp
@@ -1,100 +1,82 @@
 #include"sobel.h"
 
 #pragma design top
-void sobel_y(ac_channel<uint8> &input_image,ac_channel<uint1> &output_image)
+void sobel_y(uint8 input_image[H][W],uint1 output_image[H][W])
 {
-//directive set input_image:resc -BLOCK_SIZE 10
-	MEM_INT8 mem_in;
-	MEM_INT1 mem_out;
-	#ifndef __SYNTHESIS__
-	while(input_image.available(H*W)) 
-	#endif
-	{
-		loop_in1:for(unsigned i=0; i < H; i++) {
-			loop_in2:for(unsigned j=0; j < W; j++)
-				mem_in.data[i][j] = input_image.read();
-		}
-		int sobel_y_kernel[3][3]={{-1,0,1},{-2,0,2},{-1,0,1}};
-		int sobel_x_kernel[3][3]={{-1,-2,-1},{0,0,0},{1,2,1}};
-		int luma;
-		unsigned int pixel;
-     	unsigned int clamped;
-     	int temp;
-     	int image_reg[3][3];
-     	unsigned int b,g,r;
-	    loop_1:for (int i=1;i<H-1;i++)
-	    {
-	        loop_2:for(int j=1;j<W-1;j=j+subdim_y)
-	        {
-	
-	        image_reg[0][0]=mem_in.data[((i-1))][j-1];
-	        image_reg[0][1]=mem_in.data[((i-1))][j];
-	        image_reg[0][2]=mem_in.data[((i-1))][j+1];
-	        image_reg[1][0]=mem_in.data[i][j-1];
-	        image_reg[1][1]=mem_in.data[i][j];
-	        image_reg[1][2]=mem_in.data[i][j+1];
-	        image_reg[2][0]=mem_in.data[(i+1)][j-1];
-	        image_reg[2][1]=mem_in.data[(i+1)][j];
-	        image_reg[2][2]=mem_in.data[(i+1)][j+1];
-	
-	        loop_3:for(int k=0;k<subdim_y;k++)
-	        {
-	            int resultx=0;
-	            int resulty=0;
-	            
-	            loop_4:for (int m=0;m<3;m++)
-				{
-					loop_5:for (int n=0;n<3;n++)
-					{
-					    #pragma hls_uroll 
-						pixel = image_reg[m][n];
-						b = pixel & 0xff;
-						g = (pixel >> 8) & 0xff;
-						r = (pixel >> 16) & 0xff;
-	
-						luma = r * 66 + g * 129 + b * 25;
-						luma = (luma + 128) >> 8;
-						luma =luma + 16;
-	
-						resultx=resultx+sobel_x_kernel[m][n]*luma;
-						resulty=resulty+sobel_y_kernel[m][n]*luma;
-						//printf("%d ",pixel);
-					}
-					//printf("\n");
-				}
-	            //printf("\n");
-				resultx=resultx>0?resultx*1:resultx*-1;
-				resulty=resulty>0?resulty*1:resulty*-1;
-				temp=resultx+resulty;
-				clamped=temp>32?1:0;
-	
-				mem_out.data[i][j+k]=clamped;
-	
-				if (k+1<subdim_y)
-				{
-					//shift the reg col
-					image_reg[0][0]=image_reg[0][1];
-					image_reg[1][0]=image_reg[1][1];
-					image_reg[2][0]=image_reg[2][1];
-	
-					image_reg[0][1]=image_reg[0][2];
-					image_reg[1][1]=image_reg[1][2];
-					image_reg[2][1]=image_reg[2][2];
-					
-					image_reg[0][2]=mem_in.data[(i-1)][j+(k+1)+1];
-					image_reg[1][2]=mem_in.data[i][j+(k+1)+1];
-					image_reg[2][2]=mem_in.data[(i+1)][j+(k+1)+1];
-				}
-				if(j+k+2==W)
-					break;
-	        }
-	
-	    }
-	  }
-		loop_out1:for(unsigned i=0; i<H; i++) {
-			loop_out2:for(unsigned j=0; j<W; j++)
-				output_image.write(mem_out.data[i][j]);
-		}
-	}
+	int sobel_y_kernel[3][3]={{-1,0,1},{-2,0,2},{-1,0,1}};
+	int sobel_x_kernel[3][3]={{-1,-2,-1},{0,0,0},{1,2,1}};
+	int luma;
+	unsigned int pixel;
+ 	unsigned int clamped;
+ 	int temp;
+ 	int image_reg[3][3];
+ 	unsigned int b,g,r;
+    loop_1:for (int i=1;i<H-1;i++)
+    {
+        loop_2:for(int j=1;j<W-1;j=j+subdim_y)
+        {
 
+        image_reg[0][0]=mem_in.data[((i-1))][j-1];
+        image_reg[0][1]=mem_in.data[((i-1))][j];
+        image_reg[0][2]=mem_in.data[((i-1))][j+1];
+        image_reg[1][0]=mem_in.data[i][j-1];
+        image_reg[1][1]=mem_in.data[i][j];
+        image_reg[1][2]=mem_in.data[i][j+1];
+        image_reg[2][0]=mem_in.data[(i+1)][j-1];
+        image_reg[2][1]=mem_in.data[(i+1)][j];
+        image_reg[2][2]=mem_in.data[(i+1)][j+1];
+
+        loop_3:for(int k=0;k<subdim_y;k++)
+        {
+            int resultx=0;
+            int resulty=0;
+            
+            loop_4:for (int m=0;m<3;m++)
+			{
+				loop_5:for (int n=0;n<3;n++)
+				{
+				    #pragma hls_uroll 
+					pixel = image_reg[m][n];
+					b = pixel & 0xff;
+					g = (pixel >> 8) & 0xff;
+					r = (pixel >> 16) & 0xff;
+
+					luma = r * 66 + g * 129 + b * 25;
+					luma = (luma + 128) >> 8;
+					luma =luma + 16;
+
+					resultx=resultx+sobel_x_kernel[m][n]*luma;
+					resulty=resulty+sobel_y_kernel[m][n]*luma;
+					//printf("%d ",pixel);
+				}
+				//printf("\n");
+			}
+            //printf("\n");
+			resultx=resultx>0?resultx*1:resultx*-1;
+			resulty=resulty>0?resulty*1:resulty*-1;
+			temp=resultx+resulty;
+			clamped=temp>32?1:0;
+
+			mem_out.data[i][j+k]=clamped;
+
+			if (k+1<subdim_y)
+			{
+				//shift the reg col
+				image_reg[0][0]=image_reg[0][1];
+				image_reg[1][0]=image_reg[1][1];
+				image_reg[2][0]=image_reg[2][1];
+
+				image_reg[0][1]=image_reg[0][2];
+				image_reg[1][1]=image_reg[1][2];
+				image_reg[2][1]=image_reg[2][2];
+				
+				image_reg[0][2]=mem_in.data[(i-1)][j+(k+1)+1];
+				image_reg[1][2]=mem_in.data[i][j+(k+1)+1];
+				image_reg[2][2]=mem_in.data[(i+1)][j+(k+1)+1];
+			}
+			if(j+k+2==W)
+				break;
+        }
+    }
+  }
 }
