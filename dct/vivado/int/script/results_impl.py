964d70f22db78e7086453be18cb7f6aa404367cf
Ubuntu <ubuntu@ip-172-31-50-177.us-west-2.compute.internal>
2020-04-18 01:40:44 +0000

    Added dct int

--- dct/vivado/int/script/results_impl.py
+++ dct/vivado/int/script/results_impl.py
@@ -35,28 +35,30 @@ def parse_resources(resources_node):
     resources = [ resources_node.find(t).text for t in tags ]
     return list(map(int, resources))
 
-
 def parse_xml(filename1,filename2):
     tree = ET.parse(filename1)
     root = tree.getroot()
-
+    print(filename1)
     #resources_node       = root.find('AreaEstimates/Resources')
     #avail_resources_node = root.find('AreaEstimates/AvailableResources')
     est_clk_period = root.find('TimingReport/AchievedClockPeriod').text
-    slices=root.find('AreaReport/Resources/SLICE').text
+    slices=root.find('AreaReport/Resources/CLB').text
+    slices=int(slices)
+    lut = int(root.find('AreaReport/Resources/LUT').text)
+    ff = int(root.find('AreaReport/Resources/FF').text)
+    dsp = int(root.find('AreaReport/Resources/DSP').text)
+    bram = int(root.find('AreaReport/Resources/BRAM').text)
     tree=ET.parse(filename2)
     root=tree.getroot()
-    slices=int(slices)
     avg_latency = root.find('PerformanceEstimates/SummaryOfOverallLatency/Average-caseLatency').text
+    throughput="{0:.6f}".format(((int(avg_latency)*float(est_clk_period))/1000000000))
     #resources       = parse_resources(resources_node)
     #avail_resources = parse_resources(avail_resources_node)
-    throughput="{0:.3f}".format(((int(avg_latency)*float(est_clk_period))/1000000000))
+    
     #resources_util = np.divide(resources, avail_resources)*100
     #for i in range(4):
         #resources_util[i]="{0:.2f}".format(resources_util[i])
-    return slices,throughput
-
-
+    return slices,lut,ff,dsp,bram,throughput
 
 def removeCombinations(combs):
 
@@ -77,17 +79,17 @@ def removeCombinations(combs):
 def main():
 
     finalCombinations = removeCombinations(allCombinations)
-    file1=open('final_result_impl_dct.csv','w')
-    file1.write("n"+","+"knob_blockdim_x"+","+"knob_blockdim_y"+","+"knob_unroll_dct"+","+"knob_unroll_width"+","+"knob_unroll_height"+","+"knob_array_partition"+","+"obj1"+","+"obj2\n")
+    file1=open('vivado_dct.csv','w')
+    file1.write("n"+","+"knob_blockdim_x"+","+"knob_blockdim_y"+","+"knob_unroll_dct"+","+"knob_unroll_width"+","+"knob_unroll_height"+","+"knob_array_partition"+","+"obj1"+","+"obj2"+","+"lut"+","+"ff"+","+"dsp"+","+"bram"+"\n")
     for d in sorted(glob.glob('impl_reports/DCT_export*.xml')):
         m = re.search('DCT_export(\d+)', d)
         num = m.group(1)
         synth_path=os.path.join('syn_reports/csynth'+num+'.xml')
-        slices,lat=parse_xml(d,synth_path)
+        slices,lut,ff,dsp,bram,lat=parse_xml(d,synth_path)
         file1.write(num+",")
         for j in range(6):
             file1.write(str(finalCombinations[int(num)][j])+",")
-        file1.write(str(lat)+","+str(slices)+"\n")
+        file1.write(str(lat)+","+str(slices)+","+str(lut)+","+str(ff)+","+str(dsp)+","+str(bram)+"\n")
 
 if __name__ == "__main__":
     main()
