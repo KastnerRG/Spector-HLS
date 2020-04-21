044a6d2e3c0a8c83847affb6d8e9dc2b44bfa6b1
Ubuntu <ubuntu@ip-172-31-23-225.us-west-2.compute.internal>
2020-04-21 21:48:34 +0000

    Added mergesort asic

---
+++ mergesort/asic/results_impl_violin.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import itertools
+import os, sys
+import glob
+import re
+import numpy as np
+import xml.etree.ElementTree as ET
+
+# ***************************************************************************
+# Knobs
+# ***********
+blockdim_x =[1,2,4,8]
+blockdim_y =[1,2,4,8]
+unroll_dct=[1,2,4,8]
+unroll_width=[1,2,4,8]
+unroll_height=[1,2,4,8]
+array_partition=[1,2,4,8]
+
+
+allCombinations = list(itertools.product(
+    blockdim_x,
+    blockdim_y,
+    unroll_dct,
+    unroll_width,
+    unroll_height,
+    array_partition))
+
+
+
+# ***************************************************************************
+
+
+def parse_resources(resources_node):
+    tags = ['BRAM_18K','DSP48E','FF','LUT']
+    resources = [ resources_node.find(t).text for t in tags ]
+    return list(map(int, resources))
+
+def parse_xml(filename1,filename2):
+
+    with open(filename2, 'r') as f:
+        last_line = f.readlines()[-1]
+        last_line=last_line.split()
+        print(last_line)
+        try:
+            area=last_line[5].split('=')[1]
+            slack=last_line[8].split('=')[1]
+        except:
+            area=0
+            slack=0
+
+    f.close()
+    est_clk_period=10-float(slack)
+
+    f=open(filename1,'r')
+    print(filename1)
+    b=f.readlines()
+    k=(b[23].split())
+    print(k)
+    avg_latency=int(k[4])
+    f.close()
+    
+    throughput="{0:.6f}".format(((int(avg_latency)*float(est_clk_period))/1000000000))
+    throughput=1.0/float(throughput)
+    #resources       = parse_resources(resources_node)
+    #avail_resources = parse_resources(avail_resources_node)
+
+    #resources_util = np.divide(resources, avail_resources)*100
+    #for i in range(4):
+        #resources_util[i]="{0:.2f}".format(resources_util[i])
+    return area,throughput
+def removeCombinations(combs):
+
+    finalList = []
+
+    for c in combs:
+        copyit = True
+	
+        #if c[2]>c[0] or c[2]>c[1]:
+            #copyit =False
+
+        if copyit:
+            finalList.append(c)
+
+    return finalList
+
+
+def main():
+
+    finalCombinations = removeCombinations(allCombinations)
+    file1=open('asic_catapult_mergesort_area_violin.csv','w')
+    file1.write("Parameter"+","+"Throughput_Value"+","+"Tool"+","+"Parameter2"+","+"Area_Value"+","+"Flow"+"\n")
+    for d in sorted(glob.glob('syn_reports/cycle*.rpt')):
+        m = re.search('cycle(\d+)', d)
+        num = m.group(1)
+        log=os.path.join('syn_reports/concat_rtl.v.or'+num+'.log')
+        if os.path.isfile(log):
+            area,lat=parse_xml(d,log)
+            if area==0:
+                pass
+            else: 
+                file1.write("Throughput"+","+str(lat)+","+"catapult"+","+"Area"+","+str(area)+","+"catapult_asic_area"+"\n")
+    file1.close()
+if __name__ == "__main__":
+    main()
+        
---
+++ mergesort/catapult/asic/results_impl.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+import itertools
+import os, sys
+import glob
+import re
+import numpy as np
+import xml.etree.ElementTree as ET
+
+B=[0]
+no_size = [16, 256, 1024, 2048, 4096, 8192]
+outer_unroll = [1,2,3,4]
+inner_unroll1 = [1,2,3,4]
+inner_unroll2 = [1,2,3,4]
+merge_unroll = [1,2,3,4]
+array_part = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+
+blockCombinations = list(itertools.product(
+    no_size, #0
+    outer_unroll, #1
+    inner_unroll1, #2
+    inner_unroll2, #3
+    merge_unroll, #4
+    array_part, #5
+    B #6
+    ))
+finalCombinations=blockCombinations
+
+
+def parse_resources(resources_node):
+    tags = ['BRAM_18K','DSP48E','FF','LUT']
+    resources = [ resources_node.find(t).text for t in tags ]
+    return list(map(int, resources))
+
+
+def parse_xml(filename1,filename2):
+
+    with open(filename2, 'r') as f:
+        last_line = f.readlines()[-1]
+        last_line=last_line.split()
+        print(last_line)
+        area=last_line[5].split('=')[1]
+        slack=last_line[8].split('=')[1]
+
+    f.close()
+    est_clk_period=10-float(slack)
+
+    f=open(filename1,'r')
+    print(filename1)
+    b=f.readlines()
+    k=(b[23].split())
+    print(k)
+    avg_latency=int(k[4])
+    f.close()
+    
+    throughput="{0:.6f}".format(((int(avg_latency)*float(est_clk_period))/1000000000))
+    #resources       = parse_resources(resources_node)
+    #avail_resources = parse_resources(avail_resources_node)
+
+    #resources_util = np.divide(resources, avail_resources)*100
+    #for i in range(4):
+        #resources_util[i]="{0:.2f}".format(resources_util[i])
+    return area,throughput
+
+def removeCombinations(combs):
+
+    finalList = []
+
+    for c in combs:
+        copyit = True
+        if c[0] % c[5] != 0:
+            copyit = False
+        if c[0] / c[5] > 128:
+            copyit = False
+
+        if copyit:
+            finalList.append(c)
+
+    return finalList
+
+
+finalCombinations=removeCombinations(finalCombinations)
+
+def main():
+
+    file1=open('asic_catapult_mergesort_area.csv','w')
+    file1.write("n"+","+"knob_no_size"+","+"knob_outer_unroll"+","+"knob_inner_unroll1"+","+"knob_inner_unroll2"+","+"knob_merge_unroll"+","+"knob_array_part"+","+"knob_I_B"+","+"Latency"+","+"Area"+"\n")
+    for d in sorted(glob.glob('syn_reports/cycle*.rpt')):
+        m = re.search('cycle(\d+)', d)
+        num = m.group(1)
+        print(num)
+        log=os.path.join('syn_reports/concat_rtl.v.or'+num+'.log')
+        if os.path.isfile(log):
+            try:
+                area,lat=parse_xml(d,log)
+                file1.write(num+",")
+                for j in range(7):
+                    file1.write(str(finalCombinations[int(num)][j])+",")
+                file1.write(str(lat)+","+str(area)+"\n")
+            except:
+                pass
+        else:
+            pass
+    file1.close()
+if __name__ == "__main__":
+    main()
+        
