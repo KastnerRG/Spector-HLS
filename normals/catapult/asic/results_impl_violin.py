f75b9a8c5678b3d3c82ae79c96ff3af42fb19581
Ubuntu <ubuntu@ip-172-31-24-228.us-west-2.compute.internal>
2020-04-29 01:31:10 +0000

    Added normals asic violin

--- normals/catapult/asic/results_impl_violin.py
+++ normals/catapult/asic/results_impl_violin.py
@@ -37,6 +37,17 @@ def parse_resources(resources_node):
 
 def parse_xml(filename1,filename2):
 
+    global ff 
+    with open(filename2, 'r') as f:
+        a=f.readlines()
+    f.close() 
+    li=[x.split() for x in a]
+    for i in range(len(li)):
+        try:
+            if li[i][0]=='Number' and li[i][2]=='total':
+                ff=li[i][5]
+        except:
+            pass
     with open(filename2, 'r') as f:
         last_line = f.readlines()[-1]
         last_line=last_line.split()
@@ -54,23 +65,23 @@ def parse_xml(filename1,filename2):
     f=open(filename1,'r')
     print(filename1)
     b=f.readlines()
-    k=(b[29].split())
+    k=(b[28].split())
     print(k)
-    avg_latency=int(k[4])
+    avg_latency=int(k[3])
     f.close()
-    
+ 
     throughput="{0:.6f}".format(((int(avg_latency)*float(est_clk_period))/1000000000))
-    try:
+    if throughput !=0:
         throughput=1.0/float(throughput)
-    except:
-        throughput=0
+    else:
+        area=0
     #resources       = parse_resources(resources_node)
     #avail_resources = parse_resources(avail_resources_node)
 
     #resources_util = np.divide(resources, avail_resources)*100
     #for i in range(4):
         #resources_util[i]="{0:.2f}".format(resources_util[i])
-    return area,throughput
+    return area,throughput,ff
 def removeCombinations(combs):
 
     finalList = []
@@ -91,18 +102,17 @@ def main():
 
     finalCombinations = removeCombinations(allCombinations)
     file1=open('asic_catapult_normals_latency_violin.csv','w')
-    file1.write("Parameter"+","+"Throughput_Value"+","+"Tool"+","+"Parameter2"+","+"Area_Value"+","+"Flow"+"\n")
+    file1.write("Parameter"+","+"Throughput_Value"+","+"Tool"+","+"Parameter2"+","+"Area_Value"+","+"Parameter3"+","+"FF_Value"+","+"Flow"+"\n")
     for d in sorted(glob.glob('syn_reports/cycle*.rpt')):
         m = re.search('cycle(\d+)', d)
         num = m.group(1)
         log=os.path.join('syn_reports/concat_rtl.v.or'+num+'.log')
         if os.path.isfile(log):
-            area,lat=parse_xml(d,log)
-            print(lat)
-            if area==0 or lat==0:
+            area,lat,ff=parse_xml(d,log)
+            if area==0:
                 pass
             else: 
-                file1.write("Throughput"+","+str(lat)+","+"catapult"+","+"Area"+","+str(area)+","+"catapult_asic_latency"+"\n")
+                file1.write("Throughput"+","+str(lat)+","+"catapult"+","+"Area"+","+str(area)+","+"FF"+","+str(ff)+","+"catapult_asic_latency"+"\n")
     file1.close()
 if __name__ == "__main__":
     main()
