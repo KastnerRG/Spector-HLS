4ac617dd6c73d35f848d0b9c5003d8c9f5c243ac
Ubuntu <ubuntu@ip-172-31-23-225.us-west-2.compute.internal>
2020-04-30 04:56:41 +0000

    Added merge,hist reports

--- histogram/vivado/src/histogram_hls.h
+++
@@ -1,4 +0,0 @@
-#include "params.h"
-
-void histogram_hls(unsigned char *, unsigned int *, unsigned long);
-
--- mergesort/catapult/src/mergesort.h
+++
@@ -1,9 +0,0 @@
-#ifndef MERGESORT_H
-#define MERGESORT_H
-#include <ac_channel.h>
-#include <ac_int.h>
-#include "params.h"
-
-typedef ac_int<32, true> dint;
-
-#endif
